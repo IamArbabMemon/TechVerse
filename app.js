7a992574016df2a73c9dfd21b42d92899d7f2a3d
IamArbabMemon <139270412+IamArbabMemon@users.noreply.github.com>
2024-02-29 09:42:27 -0800

    final commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ try{
  mongoose.connect('mongodb://127.0.0.1:27017/AccessStore');   
 
  app.listen(3000,()=>{
-    console.log('Server is listening');
+    console.log('Server is listening on port 3000');
 });
 
 }catch(err){
